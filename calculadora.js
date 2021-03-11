024a97b27e41dd59c2245eae622ec3f711fe482f
Bilosca <Biloscagabriel@hotmail.com>
2021-03-10 21:16:43 -0300

    modificacoes na aparencia e funcionabilidades JavaScript

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,34 @@
 const calculadora = document.querySelector('#principal')
-const keys = calculadora.querySelector("div#botoes table tr.tr-numeros")
+const keys = calculadora.querySelector("div#botoes-area div#botoes")
+
+keys.addEventListener("click", e =>{
+    if(e.target.matches("button")){
+        const key = e.target
+        const acao = key.dataset.acao
+    
+        if(!acao){
+            console.log("numero")
+        }
+
+        if( acao === "soma" ||
+            acao === "subtrai" ||
+            acao === "multiplica" || 
+            acao === "divide"){
+                console.log("operador")
+            }
+        
+        if(acao === "calcula"){
+            console.log("igual")
+        }
+
+        if(acao === "decimal"){
+            console.log("decimal")
+        }
+
+        if(acao ==="limpa"){
+            console.log("limpa")
+        }
+    }
+})
+
 
